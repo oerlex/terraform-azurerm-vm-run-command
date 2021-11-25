3414c1ae682d1998ea023bd1056d3b1f9e11ef1d
oerlex <alex.sp@posteo.de>
2021-11-25 04:20:53 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,9 +24,9 @@ resource "azurerm_virtual_machine_extension" "linux" {
   count                      = lower(var.os_type) == "linux" ? 1 : 0
   name                       = "var.virtual_machine_name-run-command"
   #location                   = data.azurerm_resource_group.main.location
-  resource_group_name        = data.azurerm_resource_group.main.name
-  #virtual_machine_id        
-  #virtual_machine_name       = data.azurerm_virtual_machine.main.name
+  #resource_group_name        = data.azurerm_resource_group.main.name
+  virtual_machine_id         = data.azurerm_virtual_machine.main.id
+  #virtual_machine_name      = data.azurerm_virtual_machine.main.name
   publisher                  = "Microsoft.CPlat.Core"
   type                       = "RunCommandLinux"
   type_handler_version       = "1.0"
@@ -38,9 +38,10 @@ resource "azurerm_virtual_machine_extension" "linux" {
 resource "azurerm_virtual_machine_extension" "windows" {
   count                      = lower(var.os_type) == "windows" ? 1 : 0
   name                       = "var.virtual_machine_name-run-command"
-  #location                   = data.azurerm_resource_group.main.location
-  resource_group_name        = data.azurerm_resource_group.main.name
-  #virtual_machine_name       = data.azurerm_virtual_machine.main.name
+  #location                  = data.azurerm_resource_group.main.location
+  #resource_group_name       = data.azurerm_resource_group.main.name
+  #virtual_machine_name      = data.azurerm_virtual_machine.main.name
+  virtual_machine_id         = data.azurerm_virtual_machine.main.id
   publisher                  = "Microsoft.CPlat.Core"
   type                       = "RunCommandWindows"
   type_handler_version       = "1.1"
